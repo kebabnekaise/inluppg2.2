49d45b14e8e1ec4a4de5b433ebfc322499957ca7
emsi06001 <emsi06001@E-PW0234N3>
2023-09-13 13:03:43 +0200

    Add project files.

---
+++ inluppg2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace inluppg2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin?");
+            double langdElin = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Alma?");
+            double langdAlma = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Elin hoppade {langdElin - langdAlma}meter längre än Alma");
+        }
+    }
+}
